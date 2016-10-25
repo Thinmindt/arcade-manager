cab05a0abcb9380b8ef8b97bcb1687c443d2aa55
Greg <gharms@uark.edu>
2016-10-25 11:08:55 -0500

    partial Game and GamesList classes implementation

--- app/src/main/java/com/plotsandschemes/arcade/MainActivity.java
+++ app/src/main/java/com/plotsandschemes/arcade/MainActivity.java
@@ -1,8 +1,10 @@
 package com.plotsandschemes.arcade;
 
+import android.graphics.Path;
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -17,17 +19,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.TextView;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity  {
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * The {@link PagerAdapter} that will provide
      * fragments for each of the sections. We use a
      * {@link FragmentPagerAdapter} derivative, which will keep every
      * loaded fragment in memory. If this becomes too memory intensive, it
      * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     * {@link FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
@@ -35,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -77,6 +92,42 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 
     /**
@@ -107,15 +158,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            if(getArguments().getInt(ARG_SECTION_NUMBER)==1) {
+            if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
                 View rootView = inflater.inflate(R.layout.fragment_store, container, false);
                 return rootView;
             }
-            if(getArguments().getInt(ARG_SECTION_NUMBER)==2) {
+            if (getArguments().getInt(ARG_SECTION_NUMBER) == 2) {
                 View rootView = inflater.inflate(R.layout.fragment_library, container, false);
                 return rootView;
-            }
-            else {
+            } else {
                 View rootView = inflater.inflate(R.layout.fragment_community, container, false);
                 return rootView;
             }
@@ -157,6 +207,83 @@ public class MainActivity extends AppCompatActivity {
                     return "Community";
             }
             return null;
+        }
+    }
+
+
+    /**
+     * A list of games that may be retrieved from the games server.
+     * For now, the games are just listed in the class. Update when server is implemented.
+     */
+    public class AvailableGamesList {
+        private LinkedList<Game> listOfGames;
+
+        AvailableGamesList() {
+            this.listOfGames = new LinkedList<Game>();
+            getFullGamesList();
+        }
+
+        void getFullGamesList() {
+            // connect to server
+
+            // get list of games in JSON
+
+            // populate this.listOfGames
+        }
+
+        void checkForAPKs() {
+            // iterate through listOfGames
+            // check filesystem for APK
+            // if APK exists, set pathToAPK and isDownloaded
+        }
+    }
+
+
+    /**
+     * A game contains meta info and path to apk in android filesystem for each game as needed.
+     */
+    class Game {
+        private String name;
+        private Path pathToAPK;
+        private boolean isDownloaded;
+
+        Game(String name, Path pathToAPK) {
+            this.name = name;
+            this.pathToAPK = pathToAPK;
+            this.isDownloaded = true;
+        }
+
+        Game(String name) {
+            this.name = name;
+            this.pathToAPK = null;
+            this.isDownloaded = false;
+        }
+1
+        String getName() {
+            return this.name;
+        }
+
+        Path getPathToAPK() {
+            return pathToAPK;
+        }
+
+        void setPathToAPK(Path pathToAPK) {
+            this.pathToAPK = pathToAPK;
+        }
+
+        void download() {
+            // connect to server
+
+            // request game
+
+            // place in filesystem
+
+            // setPathToAPK( ... );
+            // this.isDownloaded = true;
+        }
+
+        boolean checkIfDownloaded() {
+            return isDownloaded;
         }
     }
 }
